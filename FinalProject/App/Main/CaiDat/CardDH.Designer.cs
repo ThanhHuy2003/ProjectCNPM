84550d67be8f2dd4375f7556392ab04bdcaa5d03
vuonggthanhhhuyy <52100038@student.tdtu.edu.vn>
2023-05-20 09:46:57 +0700

    met roi nha

--- FinalProject/App/Main/CaiDat/CardDH.Designer.cs
+++ FinalProject/App/Main/CaiDat/CardDH.Designer.cs
@@ -44,9 +44,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(311, 12);
+            this.label4.Location = new System.Drawing.Point(415, 15);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 25);
+            this.label4.Size = new System.Drawing.Size(183, 32);
             this.label4.TabIndex = 14;
             this.label4.Text = "Ngày đặt hàng:";
             // 
@@ -54,9 +55,10 @@
             // 
             this.lbldate.AutoSize = true;
             this.lbldate.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbldate.Location = new System.Drawing.Point(502, 12);
+            this.lbldate.Location = new System.Drawing.Point(669, 15);
+            this.lbldate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbldate.Name = "lbldate";
-            this.lbldate.Size = new System.Drawing.Size(94, 25);
+            this.lbldate.Size = new System.Drawing.Size(112, 32);
             this.lbldate.TabIndex = 13;
             this.lbldate.Text = "5/5/2023";
             // 
@@ -64,9 +66,10 @@
             // 
             this.lblStatus.AutoSize = true;
             this.lblStatus.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatus.Location = new System.Drawing.Point(311, 119);
+            this.lblStatus.Location = new System.Drawing.Point(415, 146);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(199, 25);
+            this.lblStatus.Size = new System.Drawing.Size(253, 32);
             this.lblStatus.TabIndex = 11;
             this.lblStatus.Text = "Đơn hàng thành công";
             // 
@@ -74,9 +77,10 @@
             // 
             this.lblSoluong.AutoSize = true;
             this.lblSoluong.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSoluong.Location = new System.Drawing.Point(502, 49);
+            this.lblSoluong.Location = new System.Drawing.Point(669, 60);
+            this.lblSoluong.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSoluong.Name = "lblSoluong";
-            this.lblSoluong.Size = new System.Drawing.Size(89, 25);
+            this.lblSoluong.Size = new System.Drawing.Size(112, 32);
             this.lblSoluong.TabIndex = 10;
             this.lblSoluong.Text = "Số lượng";
             // 
@@ -84,9 +88,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(311, 49);
+            this.label1.Location = new System.Drawing.Point(415, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 25);
+            this.label1.Size = new System.Drawing.Size(103, 32);
             this.label1.TabIndex = 9;
             this.label1.Text = "Số món:";
             // 
@@ -94,9 +99,10 @@
             // 
             this.lblPrice.AutoSize = true;
             this.lblPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrice.Location = new System.Drawing.Point(502, 82);
+            this.lblPrice.Location = new System.Drawing.Point(669, 101);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(66, 25);
+            this.lblPrice.Size = new System.Drawing.Size(84, 32);
             this.lblPrice.TabIndex = 16;
             this.lblPrice.Text = "Giá nè";
             // 
@@ -104,9 +110,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(311, 82);
+            this.label6.Location = new System.Drawing.Point(415, 101);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(91, 25);
+            this.label6.Size = new System.Drawing.Size(115, 32);
             this.label6.TabIndex = 15;
             this.label6.Text = "Tổng giá:";
             // 
@@ -114,8 +121,8 @@
             // 
             this.btnDetails.CornerRoundingRadius = 30F;
             this.btnDetails.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnDetails.Location = new System.Drawing.Point(1048, 107);
-            this.btnDetails.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDetails.Location = new System.Drawing.Point(1397, 132);
+            this.btnDetails.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnDetails.Name = "btnDetails";
             this.btnDetails.OverrideDefault.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(66)))));
             this.btnDetails.OverrideDefault.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(66)))));
@@ -131,7 +138,7 @@
             this.btnDetails.OverrideFocus.Border.DrawBorders = ((Krypton.Toolkit.PaletteDrawBorders)((((Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom) 
             | Krypton.Toolkit.PaletteDrawBorders.Left) 
             | Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.btnDetails.Size = new System.Drawing.Size(124, 37);
+            this.btnDetails.Size = new System.Drawing.Size(165, 46);
             this.btnDetails.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(66)))));
             this.btnDetails.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(66)))));
             this.btnDetails.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(66)))));
@@ -170,22 +177,24 @@
             | Krypton.Toolkit.PaletteDrawBorders.Right)));
             this.btnDetails.TabIndex = 96;
             this.btnDetails.Values.Text = "Chi tiết";
+            this.btnDetails.Click += new System.EventHandler(this.btnDetails_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::FinalProject.Properties.Resources.image_1;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(262, 160);
+            this.pictureBox1.Size = new System.Drawing.Size(349, 197);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
             // CardDH
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(247)))), ((int)(((byte)(229)))));
+            this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.btnDetails);
             this.Controls.Add(this.lblPrice);
             this.Controls.Add(this.label6);
@@ -195,8 +204,9 @@
             this.Controls.Add(this.lblSoluong);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CardDH";
-            this.Size = new System.Drawing.Size(1200, 160);
+            this.Size = new System.Drawing.Size(1600, 197);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
